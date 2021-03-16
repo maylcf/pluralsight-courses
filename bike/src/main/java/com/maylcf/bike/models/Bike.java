48a0d1e6145558b782b44863e9e01b4382137115
Mayara Felix <mayaralcabral@gmail.com>
2021-03-16 19:56:19 -0400

    Transform Bike into a JPA entity.

--- bike/src/main/java/com/maylcf/bike/models/Bike.java
+++ bike/src/main/java/com/maylcf/bike/models/Bike.java
@@ -1,17 +1,41 @@
 package com.maylcf.bike.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Bike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String name;
     private String email;
     private String phone;
     private String model;
     private String serialNumber;
     private BigDecimal purchasePrice;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyy")
     private Date purchaseDate;
     private boolean contact;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
