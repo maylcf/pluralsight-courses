6776da160b30f75f58a766a6b4944e3ab95f0e42
Mayara Felix <mayaralcabral@gmail.com>
2021-03-21 01:36:15 -0400

    Replace " with '.

--- bike-web-client/src/app/app.module.ts
+++ bike-web-client/src/app/app.module.ts
@@ -4,8 +4,7 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { BikeService } from "./services/bike.service";
+import { BikeService } from './services/bike.service';
 import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
