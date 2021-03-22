6cf4b60f3f2da84b1731183e70c83ad954b7523e
Mayara Felix <mayaralcabral@gmail.com>
2021-03-22 10:02:46 -0400

    Add form validation.

--- bike-web-client/src/app/components/home/home.component.ts
+++ bike-web-client/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BikeService } from '../../services/bike.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,28 @@ import { BikeService } from '../../services/bike.service';
 })
 export class HomeComponent implements OnInit {
 
+  models: string[] = [
+    'Globo MTB 29 Full Suspension',
+    'Globo Carbon Fiber Race Series',
+    'Globo Time Trial Blade'
+  ];
+  bikeform: FormGroup;
+  validMessage: string ="";
+
   constructor(private bikeService: BikeService) { }
 
   ngOnInit(): void {
+    this.bikeform = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required),
+      model: new FormControl('', Validators.required),
+      serialNumber: new FormControl('', Validators.required),
+      purchasePrice: new FormControl('', Validators.required),
+      purchaseDate: new FormControl('', Validators.required),
+      contact: new FormControl()
+    });
   }
 
+  
 }
